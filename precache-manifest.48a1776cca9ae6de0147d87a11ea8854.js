c284e9bfad13a209041ea4659dbc27dc11b3a9b5
Jin Cai <Gene@MP09.local>
2020-04-14 14:08:02 -0700

    Updates

---
+++ precache-manifest.48a1776cca9ae6de0147d87a11ea8854.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f1ca4a8e61d45ae536eabe5bd6fe0f",
+    "url": "/my_pages/index.html"
+  },
+  {
+    "revision": "aab56de0f65c321ecd68",
+    "url": "/my_pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fbb46efa8a7ec9c20f3d",
+    "url": "/my_pages/static/js/2.cdbae5e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my_pages/static/js/2.cdbae5e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aab56de0f65c321ecd68",
+    "url": "/my_pages/static/js/main.fb22eb77.chunk.js"
+  },
+  {
+    "revision": "aa92587166f5d819749a",
+    "url": "/my_pages/static/js/runtime-main.4ec7d07e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my_pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
