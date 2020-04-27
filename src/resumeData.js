791f9f71aae4587bae554c4ec249d2bcb74a9448
Jin Cai <Gene@MP09.local>
2020-04-26 17:23:20 -0700

    rbhatia46/React-Portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,74 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Gene Cai",
+    "role": "an Actuarial Software Developer",
+    
+    "roleDescription": " ",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/jin-cai-28820968/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/LittleBigGene",
+          "className":"fa fa-github"
+        }        
+      ],
+    "aboutme":"I began my career on the insurance business side as an Actarial Student. As I gain more experience, I found my deep interest in technology and fulfilling business needs for modernize/transform existing workflows and analytics.",
+    "address":"Pacific Northwest",
+    "education":[
+      {
+        "UniversityName":"University of Washington",
+        "specialization":"Statistics",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2008",
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"StanCorp Financial Group",
+        "specialization":"Supporting Actuarial Staff",
+        "MonthOfLeaving":"Since May",
+        "YearOfLeaving":"2017"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"C#"
+      },
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"SAS"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    
+  }
+  
+  export default resumeData
